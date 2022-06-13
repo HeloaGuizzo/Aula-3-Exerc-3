d61a51623750c43c1dab47131da5cbf9b5192dc6
HeloaGuizzo <107003060+HeloaGuizzo@users.noreply.github.com>
2022-06-13 19:10:31 -0300

    Adicionar arquivos de projeto.

---
+++ Aula 3 Exercício 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("----- DETERMINAR A MÉDIA DO ALUNO -----");
+
+            Console.WriteLine("Informe a 1 nota: ");
+            int nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a 2 nota: ");
+            int nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Informe a 3 nota: ");
+            int nota3 = Convert.ToInt32(Console.ReadLine());
+
+            int media = (nota1 + nota2 + nota3) / 3;
+
+            if (nota1 <= 10 && nota2 <= 10 && nota3 <= 10)
+            {
+                if (media >= 7)
+                {
+                    Console.WriteLine("Parabéns, você foi aprovado com média:" + " " + (media));
+                }
+
+                {
+                    Console.WriteLine("Que pena, você foi reprovado com média" + " " + (media));
+
+                }
+            }
+        }
+    }
+}
